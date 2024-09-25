a5e54facaa8522dac22bfd01468e660d088e2b79
pkrajenta <p.krajenta@gmail.com>
2024-09-25 20:18:27 +0200

    removed view page, count on main page

--- website/assets/js/counter.js
+++ website/assets/js/counter.js
@@ -1,8 +1,9 @@
-const counter = document.querySelector(".counter-number")
-async function updateCounter(){
-    let response = await fetch("https://ilxohwloodyhxldaua2pq6qbpu0ldfdj.lambda-url.eu-central-1.on.aws/")
+const counter = document.querySelector(".counter-number");
+
+async function updateCounter() {
+    let response = await fetch("https://ilxohwloodyhxldaua2pq6qbpu0ldfdj.lambda-url.eu-central-1.on.aws/");
     let data = await response.json();
-    CountQueuingStrategy.innerHTML = ' Views: ${data}';
+    counter.innerHTML = `Views: ${data}`;
 }
 
 updateCounter();
