75564fa4ef77446c1f70f0f0dcab8a59aaa47d67
pkrajenta <p.krajenta@gmail.com>
2024-09-25 20:40:16 +0200

    changed counter logic

--- website/assets/js/counter.js
+++ website/assets/js/counter.js
@@ -1,9 +1,17 @@
 const counter = document.querySelector(".counter-number");
 
 async function updateCounter() {
-    let response = await fetch("https://ilxohwloodyhxldaua2pq6qbpu0ldfdj.lambda-url.eu-central-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = `Views: ${data}`;
+    try {
+        let response = await fetch("https://ilxohwloodyhxldaua2pq6qbpu0ldfdj.lambda-url.eu-central-1.on.aws/");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        let data = await response.json();
+        counter.innerHTML = `Views: ${data.views}`; // Assuming data contains a 'views' field
+    } catch (error) {
+        console.error("There has been a problem with your fetch operation:", error);
+        counter.innerHTML = "Error loading views.";
+    }
 }
 
 updateCounter();
